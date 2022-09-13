1295871caff3d8a906aeb9dbdec39afe1b083cbd
AquaApple <yassershehab9@gmail.com>
2022-09-13 23:00:09 +0200

    Added MeetupList and Some Dummy Data

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Wady Houf",
+    description: "This is a first meeetup!",
+  },
+  {
+    id: "m2",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Wady Houf",
+    description: "This is a second meeetup!",
+  },
+  {
+    id: "m3",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Wady Houf",
+    description: "This is a third meeetup!",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
