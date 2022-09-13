e7065f22fc7dfee8b778342b247835155308df21
AquaApple <yassershehab9@gmail.com>
2022-09-14 00:52:06 +0200

    MongoDB and connection established

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,31 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
-  if (eq.method === "POST") {
-    const data = req.body;
+async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const data = req.body;
 
-    const { title, image, address, description } = data;
+      const client = await MongoClient.connect(
+        "mongodb+srv://AquaApple:AquaApple@cluster0.u7jyf.mongodb.net/meetups?retryWrites=true&w=majority"
+      );
+      const db = client.db();
+
+      const meetupsCollections = db.collection("meetups");
+
+      const result = await meetupsCollections.insertOne({ data });
+
+      console.log(result);
+
+      client.close();
+
+      res.status(200).json({ messaage: "Meetup inserted" });
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
-export default haanlder;
+export default handler;
